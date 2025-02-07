669986edec2e6c3abfdfb8cc34d313bac86adda6
Fabio <fabiocambulaa@yahoo.com>
2025-02-07 15:30:48 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/spesa/',
+  base: '/PortfolioFabio/spesa/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
